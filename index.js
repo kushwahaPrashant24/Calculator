924a4b4b5b00f74187d149b370fa4b85b6a64f83
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2023-11-22 00:11:10 +0530

    added some code in index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+var operatorpress = document.querySelectorAll(".operator").length;
+
+for (var i =0; i<= operatorpress; i++){
+
+
+document.querySelectorAll(".operator")[i].addEventListener("click", function () {
+    console.log("hiiii");
+});}
